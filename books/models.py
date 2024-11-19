03fcd722c4eb27247d6e33af1770f4591a9cb2fb
EdsonLopesJr <edson.eldsjr@gmail.com>
2024-11-19 17:49:56 -0300

    feat: implemeted model book

--- books/models.py
+++ books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100, verbose_name='Nome')
+    author = models.CharField(max_length=100, verbose_name='Autor')
+    published_date = models.DateField(verbose_name='Data de Publicação')
+    price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Preço')
+    pages = models.IntegerField(verbose_name='Páginas')
+     
+    class Meta:
+      ordering = ['title']
+      verbose_name = 'Livro'
+       
+    def __str__(self):
+      return self.name
+     
